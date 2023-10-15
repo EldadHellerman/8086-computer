49aea1e305bfacb484ad8e8d7806891d72bbb486
EldadHellerman <EldadHellerman@gmail.com>
2023-10-16 00:24:43 +0300

    add directives strings

--- assembler/include/assembler.h
+++ assembler/include/assembler.h
@@ -1,4 +1,12 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
 #include "isa.h"
-#include "stdlib.h"
-#include "stdio.h"
-#include "string.h"
+
+
+// Directives:
+extern const char *strings_directives[];
+extern uint8_t strings_directives_length;
+
+
+#endif /* ASSEMBLER_H */
--- assembler/src/assembler.c
+++ assembler/src/assembler.c
@@ -1,1 +1,27 @@
 #include "assembler.h"
+
+// Directives:
+
+const char *strings_directives[] = {
+    ".SECTION", // .section  SECTION_NAME
+    ".GLOBAL", // .global LABEL_NAME
+    ".EXTERN", // .extern LABEL_NAME, or like gcc, every not defiend label is extern by default
+    ".ENTRY", // .entry  LABEL_NAME, or just .entry for current location
+    ".TIMES", // .times number ... \n
+    ".EQU", // .equ identifier expression, like ".equ msg_len $-message"
+
+    ".INCBIN", // .incbin "FILE_PATH"
+    ".ALIGN", // .align number
+    ".DB", // .db list of 8-bit integers, chars, or strings
+    ".DW", // .dw list of 16-bit integers.
+    ".DD", // .dd list of 32-bit integers, or floating point numbers.
+    ".DQ", // .dq list of 64-bit integers, or double floating point numbers.
+
+    ".ORG", // .org origin_address
+    ".RESB", // .resb number_of_1_bytes
+    ".RESW", // .resw number_of_2_bytes
+    ".RESD", // .resd number_of_4_bytes
+    ".RESQ", // .resq number_of_8_bytes
+    
+};
+uint8_t strings_directives_length = sizeof(strings_directives) / sizeof(char*);
