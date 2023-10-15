f3bc330cdc3eefc0f4e391cf449041d23282702b
EldadHellerman <EldadHellerman@gmail.com>
2023-10-16 00:33:21 +0300

    - add preprocessor directives strings

--- assembler/include/preprocessor.h
+++ assembler/include/preprocessor.h
@@ -1,6 +1,12 @@
 #ifndef PREPROCESSOR_H
 #define PREPROCESSOR_H
 
+#include <inttypes.h>
+
+// Preprocessor directives:
+extern const char *strings_preprocessor_directives[];
+extern uint8_t strings_preprocessor_directives_length;
+
 void preprocessor(void);
 
 #endif /* PREPROCESSOR_H */
--- assembler/src/preprocessor.c
+++ assembler/src/preprocessor.c
@@ -1,6 +1,25 @@
+#include "preprocessor.h"
+
 #include "stdlib.h"
 #include "stdio.h"
-#include "string.h"
+
+// Preprocessor directives:
+
+const char *strings_preprocessor_directives[] = {
+    "#include",
+    "#define",
+    "#if",
+    "#ifndef",
+    "#endif",
+    "#macro",
+    "#endmacro",
+    "__FILE__",
+    "__LINE__"
+};
+
+uint8_t strings_preprocessor_directives_length = sizeof(strings_preprocessor_directives) / sizeof(char*);
+
+
 
 void preprocessor(void){
     printf("hello!\n");
